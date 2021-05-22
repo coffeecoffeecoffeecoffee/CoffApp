1366c41d9912fda3b124aa321c744c84055788c6
Michael Critz <github@michaelcritz.com>
2021-05-21 19:20:19 -0700

    Initial Commit

---
+++ Shared/CoffAppApp.swift
@@ -0,0 +1,20 @@
+//
+//  CoffAppApp.swift
+//  Shared
+//
+//  Created by Michael Critz on 5/21/21.
+//
+
+import SwiftUI
+
+@main
+struct CoffAppApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
