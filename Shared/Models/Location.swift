de4a402d5dad72fd09fd84bd5d034c6d72a90b45
Michael Critz <github@michaelcritz.com>
2021-06-01 22:29:49 -0700

    some cleanup for tv

--- Shared/Models/Location.swift
+++ Shared/Models/Location.swift
@@ -18,8 +18,12 @@ extension Location {
     func getDirections(_ name: String?) {
         guard let placemark = placeMark else { return }
         let mkLocation = MKMapItem(placemark: placemark)
+        #if os(tvOS)
+        return
+        #else
         let launchOptions = [MKLaunchOptionsDirectionsModeKey : MKLaunchOptionsDirectionsModeDriving]
         mkLocation.name = name
         mkLocation.openInMaps(launchOptions: launchOptions)
+        #endif
     }
 }
