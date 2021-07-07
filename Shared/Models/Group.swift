74281dff13192865723515c1b13da381b1e4bf39
Michael Critz <github@michaelcritz.com>
2021-07-06 21:40:47 -0700

    Added an API tool to consume site endpoints

--- Shared/Models/Group.swift
+++ Shared/Models/Group.swift
@@ -16,19 +16,3 @@ extension Group {
         URL(string: "https://coffeecoffeecoffee.coffee/api/groups/\(id.uuidString.lowercased())/events")
     }
 }
-
-extension Group {
-    func setSelected() {
-        if let encoded = try? JSONEncoder().encode(self) {
-            UserDefaults.sharedSuite.setValue(encoded, forKey: UserDefaultKeys.selectedGroup.rawValue)
-        }
-    }
-
-    static func loadSelected() -> Group? {
-        if let savedData = UserDefaults.sharedSuite.data(forKey: UserDefaultKeys.selectedGroup.rawValue),
-           let savedGroup = try? JSONDecoder().decode(Group.self, from: savedData) {
-            return savedGroup
-        }
-        return nil
-    }
-}
