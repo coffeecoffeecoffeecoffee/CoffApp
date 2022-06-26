343ed2f0fcc03436283eea4eacfd96ccb5084e0c
Michael Critz <github@michaelcritz.com>
2022-06-25 21:32:16 -0700

    Adds transition to AsyncImagePhaseView

--- Shared/Views/AsyncImagePhaseView.swift
+++ Shared/Views/AsyncImagePhaseView.swift
@@ -4,15 +4,18 @@ struct AsyncImagePhaseView: View {
     let url: URL?
 
     var body: some View {
-        AsyncImage(url: url) { phase in
+        AsyncImage(url: url,
+                   transaction: .init(animation: .easeIn(duration: 0.7))) { phase in
             switch phase {
             case .success(let img):
                 img.resizable()
                     .centerCropped()
+                    .transition(.opacity)
             default:
                 Image("coffee-\(Int.random(in: 1...10))")
                     .resizable()
                     .centerCropped()
+                    .transition(.opacity)
             }
         }
     }
